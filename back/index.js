c42c826c4566cb543fe66f797ae8f21d6f2087f3
Lucas <lucassarachu7@gmail.com>
2023-12-22 18:58:09 +0100

    Creando servidor

--- back/index.js
+++ back/index.js
@@ -1,11 +1,21 @@
 import express  from "express";
+import dotenv from 'dotenv';
+ 
+import connectDB from "./config/db.js";
 
 const app = express()
+
+dotenv.config()
+
+connectDB()
+
 
 app.use('/', (req, res) => {
      
 })
 
-app.listen(4000, () => {
-    console.log('Servidor funcionando')
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en ${PORT}`)
 })
