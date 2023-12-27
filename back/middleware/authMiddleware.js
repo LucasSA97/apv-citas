a510274e8db4b6540d2ef5d27cd7ac5ada480bd7
Lucas <lucassarachu7@gmail.com>
2023-12-27 10:33:24 +0100

    Validando y autenticando con JWT

--- back/middleware/authMiddleware.js
+++ back/middleware/authMiddleware.js
@@ -1,10 +1,27 @@
-const checkAuth = (req, res, next) => {
+import jwt from "jsonwebtoken"
+import Veterinario from "../models/Veterinario.js"
 
+const checkAuth = async (req, res, next) => {
+    let token;
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        console.log('Tiene el token con bearer')
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+             req.veterinario = await Veterinario
+             .findById(decoded.id)
+             .select("-password -token -confirmado")
+            return next()
+        } catch (error) {
+            const err = new Error('Token no valido')
+            return res.status(403).json({ msg: err.message }) 
+     }
     } 
-    const error = new Error('Token no valido')
-    res.status(403).json({ msg: error.message })
+
+    if(!token){
+        const error = new Error('Token no valido o inexistente')
+        res.status(403).json({ msg: error.message })
+    }
+
     next()
 }
 
