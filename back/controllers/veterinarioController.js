f3d7630b73219af87e4225e433d04a98979d9dd1
Lucas <lucassarachu7@gmail.com>
2023-12-27 10:39:14 +0100

    Accediendo al usuario autenticado

--- back/controllers/veterinarioController.js
+++ back/controllers/veterinarioController.js
@@ -24,7 +24,8 @@ const registrar = async (req, res) => {
 }
 
 const perfil = (req, res) => {
-    res.json({ msg: 'Mostrando perfil'})
+    const { veterinario } = req
+    res.json({ perfil: veterinario })
 }
 
 const confirmar = async (req, res) => {
