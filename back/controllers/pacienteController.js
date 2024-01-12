a999ea61fdbc87fde9bf0f58de66b44c23811085
Lucas <lucassarachu7@gmail.com>
2024-01-12 14:16:59 +0100

    Actualizar paciente

--- back/controllers/pacienteController.js
+++ back/controllers/pacienteController.js
@@ -19,20 +19,48 @@ export const obtenerPacientes = async ( req, res ) =>{
 
     res.json(pacientes)
 }
+
 export const obtenerPaciente = async ( req, res ) =>{
     const { id } = req.params
     const paciente = await Paciente.findById(id)
+
+    if(!paciente){
+        return res.status(404).json({msg: 'No encontrado'})
+      }
+
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
+         return res.json({msg: 'Accion no valida'})
+    }
+    // if(paciente){
+    //     res.json(paciente)
+    // }
+}
+
+export const actualizarPaciente = async ( req, res ) =>{
+    const { id } = req.params
+    const paciente = await Paciente.findById(id)
+
+    if(!paciente){
+        return res.status(404).json({msg: 'No encontrado'})
+      }
 
     if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()){
          return res.json({msg: 'Accion no valida'})
     }
 
-    if(paciente){
-        res.json(paciente)
+    //Actualizamos pacientes
+    paciente.nombre = req.body.nombre || paciente.nombre
+    paciente.propietario = req.body.propietario || paciente.propietario
+    paciente.email = req.body.email || paciente.email
+    paciente.fecha = req.body.fecha || paciente.fecha
+    paciente.sintomas = req.body.sintomas || paciente.sintomas
+    try {
+        const pacienteActualizado = await paciente.save()
+        res.json(pacienteActualizado)
+    } catch (error) {
+        console.log(error)
     }
-}
-
-export const actualizarPaciente = async ( req, res ) =>{
+    
 
 }
 
