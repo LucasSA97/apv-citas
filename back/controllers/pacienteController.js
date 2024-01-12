96aea72f12d0063499a284693bf3fdbd0c4221ef
Lucas <lucassarachu7@gmail.com>
2024-01-12 12:38:37 +0100

    Comprobando que se almacene correctamente parte2

--- back/controllers/pacienteController.js
+++ back/controllers/pacienteController.js
@@ -1,10 +1,11 @@
 import Paciente from '../models/Paciente.js'
 
-export const agregarPaciente = ( req, res ) =>{
+export const agregarPaciente = async ( req, res ) =>{
 const paciente = new Paciente( req.body )
- 
+ paciente.veterinario = req.veterinario._id
 try {
-    
+    const pacienteAlmacenado = await paciente.save()
+    res.json(pacienteAlmacenado)
 } catch (error) {
     console.log(error)
 }
