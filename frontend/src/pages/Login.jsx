1146e23c3c19b8bbaff76eb9dd36f93fd10d1d34
Lucas <lucassarachu7@gmail.com>
2024-01-14 20:03:07 +0100

    Creando formulario de Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,11 +3,36 @@ const Login = () => {
   return (
     <>
          <div>
-            <h1 className="text-indigo-700  font-black text-xl">Inicia Sesión y Administra tus <span className="text-black font-black">Pacientes</span></h1>
+            <h1 className="text-indigo-700  font-black text-6xl">Inicia Sesión y Administra tus <span className="text-black font-black">Pacientes</span></h1>
         </div>
 
         <div>
-            <h1>2</h1>
+            <form action="">
+                <div className="my-5">
+                    <label 
+                    className="uppercase text-grey-600 block text-xl font-bold">
+                        Email
+                    </label>
+                    <input 
+                    className="border w-full p-3 mt-3 bg-green-200 rounded-lg" 
+                    type="email" 
+                    placeholder="Email de Registro"  />
+                </div>
+                <div className="my-5">
+                    <label 
+                    className="uppercase text-grey-600 block text-xl font-bold">
+                        Password</label>
+                    <input
+                     className="border w-full p-3 mt-3 bg-green-200 rounded-lg" 
+                     type="password" 
+                     placeholder="Password"/>
+                </div>
+                <input 
+                type="submit"
+                value="Iniciar Sesión"
+                className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto " />
+
+            </form>
          </div>
        
     </>
