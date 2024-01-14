c4a31a793668deefdc61e85914f0af7d90622c7f
Lucas <lucassarachu7@gmail.com>
2024-01-14 19:49:23 +0100

    Creando componente Login

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-        <h1>Administrador de Pacientes de Veterinaria </h1>
+        <main className="container mx-auto md:grid md:grid-cols-2 mt-11">
+            <Outlet/>
+        </main>
 
-        <Outlet/>
-        
     </>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,14 @@
 const Login = () => {
   return (
     <>
-        <h2>Login</h2>
+         <div>
+            <h1 className="text-indigo-700  font-black text-xl">Inicia Sesión y Administra tus <span className="text-black font-black">Pacientes</span></h1>
+        </div>
+
+        <div>
+            <h1>2</h1>
+         </div>
+       
     </>
   )
 }
