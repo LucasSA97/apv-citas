7a12c23512466563ad606b5132fa112999da79f3
Lucas <lucassarachu7@gmail.com>
2024-01-20 12:22:38 +0100

    Haciendo responsive la app

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <head className="py-10 bg-indigo-600">
-        <div className="container mx-auto flex justify-between items-center">
-            <h1 className="font-bold text-2xl text-indigo-200">Administrador de Pacientes de {''} <span className="text-white font-black">Veterinaria</span></h1>
+        <div className="container mx-auto flex justify-between items-center flex-col lg:flex-row">
+            <h1 className="font-bold text-2xl text-center text-indigo-200">Administrador de Pacientes de {''} <span className="text-white font-black">Veterinaria</span></h1>
 
-            <nav className="flex gap-4">
+            <nav className="flex flex-col items-center lg:flex-row gap-4 mt-5 lg:mt-0">
                 <Link to='/admin' className="text-white text-xl font-bold">Pacientes</Link>
                 <Link to='/admin' className="text-white text-xl">Perfil</Link>
                 <button 
--- frontend/src/layout/PrivateZone.jsx
+++ frontend/src/layout/PrivateZone.jsx
@@ -11,7 +11,11 @@ const PrivateZone = () => {
     <>
         <Header/>
 
-         { auth?._id ? <Outlet/> : <Navigate to="/"/>}
+         { auth?._id ? (
+            <main className="container mx-auto mt-10">
+                <Outlet/>
+            </main>
+         ) : <Navigate to="/"/>}
 
         <Footer/>
     </>
