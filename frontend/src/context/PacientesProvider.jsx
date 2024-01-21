bc6e5aa17c7a1fba945c4edfd7f681da4ede6785
Lucas <lucassarachu7@gmail.com>
2024-01-21 13:16:30 +0100

    Obtener pacientes del usuario

--- frontend/src/context/PacientesProvider.jsx
+++ frontend/src/context/PacientesProvider.jsx
@@ -7,6 +7,27 @@ export const PacientesProvider = ({children}) => {
 
     const [ pacientes, setPacientes ] = useState([])
 
+    useEffect(() => {
+        const obtenerPacientes = async () => {
+            try {
+                const token = localStorage.getItem('token')
+                if(!token) return
+
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            const { data } = await clienteAxios('/pacientes', config)
+            setPacientes(data)
+            } catch (error) {
+                
+            }
+        }
+        obtenerPacientes()
+    }, [])
+
     const savePaciente = async (paciente) => {
         try {
             const token = localStorage.getItem('token')
